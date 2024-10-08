82fb6ee0cb2de713ede3b982b1020e6923db5c1e
Rufep <rubfport84@gmail.com>
2024-10-08 19:35:24 +0200

    while ejemplo terminado

--- Clase 4/Sentencias De Repetición/Bucle While/BucleWhile.java
+++ Clase 4/Sentencias De Repetición/Bucle While/BucleWhile.java
@@ -22,6 +22,7 @@ public class BucleWhile {
 		//condicion de continuidad(mientras la condicion se cumpla el bucle se va a seguir ejecutando y cuando deje de cumplirse el bucle termina)
 		
 		int i = 1;//variable de control 
+		
 		/**
 		 * mientras i sea menor que el valor introducido por el usuario por teclado
 		 * pinta un asterisco
@@ -35,11 +36,51 @@ public class BucleWhile {
 
 		//sumar todos los valores almacenados en una Matriz
 		System.out.println("Siguiente apartado");
+		System.out.println();
+		
+		int valores[] = {7,9,4,5,2,6,9,2,3,1};
+		
+		int suma = 0;
+		
+		int posicion=0;
+		
+		while(posicion < valores.length){
+			
+			suma = suma +valores[posicion];
+			posicion++;
+			
+		}
+		System.out.println("El resultado de la suma de la matriz es: "+ suma);
 		
 		
+		//Prograna que cuente cuantas veces aparece un caracter en un String
 		
+		System.out.println("Siguiente Apartado");
+		System.out.println();
 		
+		String frase = "El Celta gano con nueve jugadores";
 		
+		//contar cuantas veces aparece la letra 'a'
+			
+			char letra ='a';
+			int contador=0;
+			
+			int j = 0; //variable de control
+			
+			while(j<frase.length()) {
+				if(frase.charAt(j)==letra) {
+					contador++;
+				}
+					j++;
+			}
+			if(contador == 0) {
+				System.out.println(letra + " No esta en la frase");
+			}else if(contador == 1) {
+				System.out.println(letra + " Aparece 1 vez");
+			}else {
+				System.out.println(letra + " Aparece " + contador + " veces");
+			}
+			
 	}
-
+	
 }
