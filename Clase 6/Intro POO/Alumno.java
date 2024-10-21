f632dffbce32e6e4397d4e25a393ffc4db9705f0
Rufep <rubfport84@gmail.com>
2024-10-21 20:04:34 +0200

    intro POO

--- Clase 6/Intro POO/Alumno.java
+++ Clase 6/Intro POO/Alumno.java
@@ -2,4 +2,41 @@ package introPOO;
 
 public class Alumno {
 
+	//DNI,Nombre,Email,Edad
+	
+	String dni;
+	String nombre;
+	String email;
+	int edad;
+	float notas[];//inicializar este Array
+	Curso curso;
+	
+	//constructor por defecto
+	public Alumno() {
+		dni=null;
+		nombre=null;
+		email=null;
+		edad=0;
+		notas=new float[10]; // inicializaciojn del array
+		//añadir notas al array
+		for(int i =0 ; i<notas.length; i++) {
+			notas[i]=-1;
+		}
+		
+	}
+	
+	//añadir notas a un  alumno
+	
+	public void addNota(float nota){
+		for(int i=0; i<notas.length;i++) {
+			if(notas[i]==-1) {
+				notas[i]=nota;
+				break;
+			}
+		}
+		
+		
+	}
+	
+	
 }
--- Clase 6/Intro POO/TestAlumno.java
+++ Clase 6/Intro POO/TestAlumno.java
@@ -5,12 +5,26 @@ public class TestAlumno {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Declarar variables de tipo Alumno
+		//Declarar variables de tipo Alumno(objeto)
 		
 		Alumno alum1;
 		
-		//creamos un alumno(Instanciamos un objeto de tipo alumno en Memoria)
+		
+		//creamos un alumno(Instanciamos un objeto de tipo alumno en Memoria?)
 		alum1 = new Alumno();
+		
+		//declaracion y creacion 
+		Alumno alum2 = new Alumno();
+		
+		//modificar los datos del amlumno alum1 y alum2
+		
+		alum1.dni= "123456789A";
+		
+		alum2.dni="987654321B";
+		
+		//mostrar por pantalla
+		
+		System.out.println();
 	}
 
 }
